e34d90d5c0247ab5c1dbc64939091f42eb2c8143
Carlos Dutra <kduitba@gmail.com>
2025-09-03 19:23:03 -0300

    Comentário de atualização

---
+++ BOOTCAMPS DIO/operadores_aritmeticos.py
@@ -0,0 +1,21 @@
+produto1 = 25
+produto2 = 10
+print(produto1 + produto2) # adição
+print(produto1 - produto2) # subtração
+print(produto1 * produto2) # multiplicação
+print(produto1 / produto2) # divisão
+print(produto1 // produto2) # divisão inteira
+print(produto1 ** produto2) # exponenciação
+print(produto1 % produto2) # módulo (resto da divisão)
+
+x = 10 + (5 * 4)
+print(x)
+
+y = (10 + 5) * 4
+print(y)
+
+z = (10 / 2) + (25 * 2) - (2 ** 2)
+print(z)
+
+w = (10 / 2) + 25 * ((2 - 2) ** 2)
+print(w)
---
+++ BOOTCAMPS DIO/operadores_comparacoes.py
@@ -0,0 +1,16 @@
+# operadores de comparações retornam tipo de dado booleano, verdadeiro ou falso
+# == igual
+# != diferente
+# > maior
+# >= maior ou igual
+# < menor
+# <= menor ou igual
+
+saldo = 200
+saque = 200
+print(saldo == saque)
+print(saldo != saque)
+print(saldo > saque)
+print(saldo >= saque)
+print(saldo < saque)
+print(saldo <= saque)
