b8e1b2fd5e50e47bbeceba9429086b965da4a16c
Carlos Dutra <kduitba@gmail.com>
2025-09-11 20:22:31 -0300

    Comentário de atualização

---
+++ BOOTCAMPS DIO/estruturas_repeticoes_break.py
@@ -0,0 +1,17 @@
+while True:
+  numero = int(input("Informe um número: "))
+  if numero ==10:
+    break
+  print(numero)
+
+
+for numero in range(100):
+  if numero == 10:
+    break
+  print(numero, end=" ")
+
+
+for numero in range(100):
+  if numero == 10:
+    continue
+  print(numero, end=" ")
---
+++ BOOTCAMPS DIO/estruturas_repeticoes_range.py
@@ -0,0 +1,2 @@
+for numero in range(0, 51, 5):
+  print(numero, end=" ")  # o end fiz imprimir o resultado somente em uma linha
